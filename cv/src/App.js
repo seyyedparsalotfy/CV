f237d4dabaa00a5b67af40d64bfdbf33205abc26
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2024-02-21 12:28:59 +0000

    p

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,8 +1,31 @@
+import React, { useEffect, useState } from "react";
 
-import './App.css';
+const PdfViewer = ({ pdfUrl }) => {
+  useEffect(() => {
+    const refreshPage = setTimeout(() => {
+      window.location.reload();
+    }, 10000);
 
-function App() {
- 
-}
+    return () => clearTimeout(refreshPage);
+  }, []);
+
+  return (
+    <embed src={pdfUrl} width="100%" height="100%" type="application/pdf" />
+  );
+};
+
+const App = () => {
+  const [isFirstPdf, setIsFirstPdf] = useState(true);
+
+  return (
+    <div>
+      {isFirstPdf ? (
+        <PdfViewer pdfUrl="cv/cv/public/Parsa.pdf" />
+      ) : (
+        <PdfViewer pdfUrl="cv/cv/public/CV.32.pdf" />
+      )}
+    </div>
+  );
+};
 
 export default App;
